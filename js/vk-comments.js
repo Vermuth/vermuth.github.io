15b1e06f5bb2a0024e198b6bb458301b8a0cf50f
Damir Makhmutov <damir.makhmutov@headmade.pro>
2016-10-23 17:50:45 +0300

    Remove vk-comments vidget, add css mobile support

--- js/vk-comments.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-  VK.init({apiId: 5080735, onlyWidgets: true});
-  VK.Widgets.Comments("vk_comments", {limit: 10, width: "800", attach: "*"});
-});
--- js/vk_vidget.js
+++ js/vk_vidget.js
@@ -1,7 +1,17 @@
 $(function() {
-  VK.Widgets.Group("vk_groups", {
-  	mode: 2,
-  	width: "auto",
-  	height: "500",
-  }, 51047651);
+  function VK_Widget_Init(){
+    document.getElementById('vk_groups').innerHTML = "";
+
+    var vk_width = document.getElementById('vk_groups').clientWidth;
+    // var vk_width = document.getElementById('vk_widget').clientWidth;
+
+    VK.Widgets.Group("vk_groups", {
+      mode: 2,
+      width: vk_width,
+      height: "500",
+    }, 51047651);
+  };
+
+  window.addEventListener('load', VK_Widget_Init, false);
+  window.addEventListener('resize', VK_Widget_Init, false);
 });
